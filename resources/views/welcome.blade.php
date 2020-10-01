3f836bf5686dd456eb8303daaf017cf0408856c0
prabhatjoshi321 <57354589+prabhatjoshi321@users.noreply.github.com>
2020-10-01 16:54:17 +0530

    http error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
