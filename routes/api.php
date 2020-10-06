950fd693122a116ffbb83123191d84af842a893d
prabhatjoshi321 <57354589+prabhatjoshi321@users.noreply.github.com>
2020-10-07 01:54:38 +0530

    onsumer Login registration

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('/login', 'App\Http\Controllers\Api\AuthController@login');
+    Route::post('/signup', 'App\Http\Controllers\Api\AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('/logout', 'App\Http\Controllers\Api\AuthController@logout');
+        Route::get('/user', 'App\Http\Controllers\Api\AuthController@user');
+    });
+    Route::get('/home', 'App\Http\Controllers\Api\HomeController@index')->name('home');
 });
